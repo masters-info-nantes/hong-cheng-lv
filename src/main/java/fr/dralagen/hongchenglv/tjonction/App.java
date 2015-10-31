785106fb2b49cb559a5a723b008ddac9024d310c
Dralagen <dralagen@dralagen.fr>
2015-10-31 15:49:16 +0100

    fixes mainclass on manifest and fixes app resources location

--- src/main/java/fr/dralagen/hongchenglv/tjonction/App.java
+++ src/main/java/fr/dralagen/hongchenglv/tjonction/App.java
@@ -12,97 +12,112 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class App extends JFrame{
-	
-	public App(){
-		super("Hong cheng lv");
-		
-		WindowListener l = new WindowAdapter() {
-			public void windowClosing(WindowEvent e){
-				System.exit(0);
-			}
-		};
-		setLayout(null);
-		addWindowListener(l);
-		setSize(430, 750);
-		setVisible(true);
-		//setResizable(false);
-		
-		Tjonction crossing = new Tjonction();
+public class App extends JFrame {
 
-		Button startStop = new Button("Stop");
-		startStop.addActionListener(new ActionListener() {
-			
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				if(startStop.getLabel().equals("Stop")){
-					startStop.setLabel("Start");
-				       crossing.quit();
-				}
-				else{
-					startStop.setLabel("Stop");
-				}
-				
-			}
-		});
-		
-		
-		add(startStop);
-		startStop.setBounds(0, 0, 100, 50);
-		
-		ImageIcon majorRouteTop = new ImageIcon("ressources/img/routeV.gif");
-		ImageIcon majorRouteDown = new ImageIcon("ressources/img/routeV.gif");
-		ImageIcon minorRoute = new ImageIcon("ressources/img/route.gif");
-		ImageIcon intersection = new ImageIcon("ressources/img/intersection.gif");
+    public App() {
+        super("Hong cheng lv");
 
-		JLabel labMajorRouteTop = new JLabel();
-		labMajorRouteTop.setIcon(majorRouteTop);
-		
-		JLabel labMinorRoute = new JLabel();
-		labMinorRoute.setIcon(minorRoute);
-		
-		add(labMajorRouteTop);
-		labMajorRouteTop.setBounds(minorRoute.getIconWidth(),0, majorRouteTop.getIconWidth(), majorRouteTop.getIconHeight());
-		labMajorRouteTop.setLocation(minorRoute.getIconWidth(), 0);
+        WindowListener l = new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        };
+        setLayout(null);
+        addWindowListener(l);
+        setSize(430, 750);
+        setVisible(true);
+        // setResizable(false);
 
-		add(labMinorRoute);
+        Tjonction crossing = new Tjonction();
 
-		labMinorRoute.setBounds(0,labMajorRouteTop.getHeight(), minorRoute.getIconWidth(), minorRoute.getIconHeight());
-		
-		
-		JLabel MinorFeu = crossing.getMinor().getFeu();
-		add(MinorFeu);
-		MinorFeu.setLocation(labMinorRoute.getX() + labMinorRoute.getWidth() - MinorFeu.getWidth(), labMinorRoute.getY() + labMinorRoute.getHeight());
+        Button startStop = new Button("Stop");
+        startStop.addActionListener(new ActionListener() {
 
-		JLabel MajorFeuTop = crossing.getMajor().getFeu1();
-		add(MajorFeuTop);
-		MajorFeuTop.setLocation(labMajorRouteTop.getX() - MajorFeuTop.getWidth() , labMajorRouteTop.getY() + labMajorRouteTop.getHeight() - MinorFeu.getHeight());
-		
-		
-		JLabel labIntersection = new JLabel();
-		labIntersection.setIcon(intersection);
-		add(labIntersection);
-		labIntersection.setBounds(labMinorRoute.getX() + minorRoute.getIconWidth(), labMinorRoute.getY(), intersection.getIconHeight(), intersection.getIconHeight());
-		
-		JLabel labMajorRouteDown = new JLabel();
-		labMajorRouteDown.setIcon(majorRouteDown);
-		add(labMajorRouteDown);
-		labMajorRouteDown.setBounds(labIntersection.getX(), labIntersection.getY() + labIntersection.getHeight(), majorRouteDown.getIconWidth(), majorRouteDown.getIconHeight());
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+                if (startStop.getLabel().equals("Stop")) {
+                    startStop.setLabel("Start");
+                    crossing.quit();
+                } else {
+                    startStop.setLabel("Stop");
+                }
 
-		JLabel MajorFeuDown = crossing.getMajor().getFeu2();
-		add(MajorFeuDown);
-		MajorFeuDown.setLocation(labMajorRouteDown.getX() + labMajorRouteDown.getWidth(), labMajorRouteDown.getY());
-		
-		crossing.start();
-		
-	}
+            }
+        });
 
-	public static void main(String[] args) {
-		JFrame frame = new App();
-		frame.setBackground(Color.BLACK);
+        add(startStop);
+        startStop.setBounds(0, 0, 100, 50);
 
-		
-	}
+        ImageIcon majorRouteTop = new ImageIcon(
+                getClass().getResource("/img/routeV.gif"));
+        ImageIcon majorRouteDown = new ImageIcon(
+                getClass().getResource("/img/routeV.gif"));
+        ImageIcon minorRoute = new ImageIcon(
+                getClass().getResource("/img/route.gif"));
+        ImageIcon intersection = new ImageIcon(
+                getClass().getResource("/img/intersection.gif"));
+
+        JLabel labMajorRouteTop = new JLabel();
+        labMajorRouteTop.setIcon(majorRouteTop);
+
+        JLabel labMinorRoute = new JLabel();
+        labMinorRoute.setIcon(minorRoute);
+
+        add(labMajorRouteTop);
+        labMajorRouteTop.setBounds(minorRoute.getIconWidth(), 0,
+                majorRouteTop.getIconWidth(), majorRouteTop.getIconHeight());
+        labMajorRouteTop.setLocation(minorRoute.getIconWidth(), 0);
+
+        add(labMinorRoute);
+
+        labMinorRoute.setBounds(0, labMajorRouteTop.getHeight(),
+                minorRoute.getIconWidth(), minorRoute.getIconHeight());
+
+        JLabel MinorFeu = crossing.getMinor().getFeu();
+        add(MinorFeu);
+        MinorFeu.setLocation(
+                labMinorRoute.getX() + labMinorRoute.getWidth()
+                        - MinorFeu.getWidth(),
+                labMinorRoute.getY() + labMinorRoute.getHeight());
+
+        JLabel MajorFeuTop = crossing.getMajor().getFeu1();
+        add(MajorFeuTop);
+        MajorFeuTop.setLocation(
+                labMajorRouteTop.getX() - MajorFeuTop.getWidth(),
+                labMajorRouteTop.getY() + labMajorRouteTop.getHeight()
+                        - MinorFeu.getHeight());
+
+        JLabel labIntersection = new JLabel();
+        labIntersection.setIcon(intersection);
+        add(labIntersection);
+        labIntersection.setBounds(
+                labMinorRoute.getX() + minorRoute.getIconWidth(),
+                labMinorRoute.getY(), intersection.getIconHeight(),
+                intersection.getIconHeight());
+
+        JLabel labMajorRouteDown = new JLabel();
+        labMajorRouteDown.setIcon(majorRouteDown);
+        add(labMajorRouteDown);
+        labMajorRouteDown.setBounds(labIntersection.getX(),
+                labIntersection.getY() + labIntersection.getHeight(),
+                majorRouteDown.getIconWidth(), majorRouteDown.getIconHeight());
+
+        JLabel MajorFeuDown = crossing.getMajor().getFeu2();
+        add(MajorFeuDown);
+        MajorFeuDown.setLocation(
+                labMajorRouteDown.getX() + labMajorRouteDown.getWidth(),
+                labMajorRouteDown.getY());
+
+        crossing.start();
+
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new App();
+        frame.setBackground(Color.BLACK);
+
+    }
 
 }
--- src/main/java/fr/dralagen/hongchenglv/tjonction/Road.java
+++ src/main/java/fr/dralagen/hongchenglv/tjonction/Road.java
@@ -14,29 +14,30 @@ public class Road extends Thread {
 
     protected boolean isPower;
 
-	JLabel feu1;
-	JLabel feu2;
-	
+    JLabel feu1;
+    JLabel feu2;
+
     public Road(TrafficLight light, Lock lock) {
         trafficLight = light;
         greenLocker = lock;
-        ImageIcon img = new ImageIcon("ressources/img/feux-feuvert.gif");
-		feu1 = new JLabel();
-		feu1.setIcon(img);
-		feu1.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
-		
-		feu2 = new JLabel();
-		feu2.setIcon(img);
-		feu2.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
-		
+        ImageIcon img = new ImageIcon(
+                getClass().getResource("/img/feux-feuvert.gif"));
+        feu1 = new JLabel();
+        feu1.setIcon(img);
+        feu1.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
+
+        feu2 = new JLabel();
+        feu2.setIcon(img);
+        feu2.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
+
     }
 
     public void toStop() {
         trafficLight.setState(StateLight.YELLOW);
-        ImageIcon img = new ImageIcon("ressources/img/feux-feuorange.gif");
-		feu1.setIcon(img);
-		feu2.setIcon(img);
-
+        ImageIcon img = new ImageIcon(
+                getClass().getResource("/img/feux-feuorange.gif"));
+        feu1.setIcon(img);
+        feu2.setIcon(img);
 
         try {
             Thread.sleep(1000);
@@ -44,17 +45,18 @@ public class Road extends Thread {
         }
 
         trafficLight.setState(StateLight.RED);
-        img = new ImageIcon("ressources/img/feux-feurouge.gif");
-		feu1.setIcon(img);		
-		feu2.setIcon(img);
+        img = new ImageIcon(getClass().getResource("/img/feux-feurouge.gif"));
+        feu1.setIcon(img);
+        feu2.setIcon(img);
 
     }
 
     public void toOpen() {
         trafficLight.setState(StateLight.GREEN);
-        ImageIcon img = new ImageIcon("ressources/img/feux-feuvert.gif");
-		feu1.setIcon(img);
-		feu2.setIcon(img);
+        ImageIcon img = new ImageIcon(
+                getClass().getResource("/img/feux-feuvert.gif"));
+        feu1.setIcon(img);
+        feu2.setIcon(img);
 
     }
 
